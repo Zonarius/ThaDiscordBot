ac20fba1f6586d795776a88b512a122a110dc776
tharyo - <tharyo@hostinger.de>
2017-02-25 15:13:05 +0100

    Agressive pong

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ client.on('voiceStateUpdate', (before, after) => {
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-        msg.reply('Pong!!!');
+        msg.reply('Pong!!!!');
     }
 });
 
